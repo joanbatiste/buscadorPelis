ac35452135fa4adb40a409a144264f446afe77d7
Joan <joanpulle@gmail.com>
2021-02-16 17:35:03 +0100

    busqueda por id funcionando, fea pero funciona

--- js/main.js
+++ js/main.js
@@ -17,26 +17,39 @@ let base_url = `http://api.themoviedb.org/3/`;
 const call = async (url) => {
     let res = await axios.get(url);
     
-    if(!res.data.results){
-        error = new Error("La url era incorrecta");
-        return error;
+
+    if (res.data.results){
+        return res.data.results;
+
+    }
+    if (res.data.title){
+        return res.data;
     }
     
-    return res.data.results;
 }
+
 
 const pintar = async (coleccionPintar) => {
     //Proceso para el pintado HTML de las películas
     const divPelisDomElement = document.getElementById("contenedor");
     
-    console.log(coleccionPintar);
+    // console.log(coleccionPintar);
     if (Array.isArray(coleccionPintar)) {
         coleccionPintar.forEach(pelicula => {
-            divPelisDomElement.innerHTML += `<div class='Peliculas'><img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' width='200px' class='Picture'></img></div><div class='infoPelis'><h2>${pelicula.title}</h2><p>${pelicula.overview}</p></div>`
+            divPelisDomElement.innerHTML += `<div class='Peliculas'>
+            <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' width='200px' class='Picture'>
+            </img></div><div class='infoPelis'><h2>${pelicula.title}</h2><p>${pelicula.overview}</p></div>`
         });
-    }else{
-
-    };
+    }
+    
+    if (coleccionPintar.original_title){
+        divPelisDomElement.innerHTML += `<div class='Peliculas'>
+            <img src='https://image.tmdb.org/t/p/w500${coleccionPintar.poster_path}' width='200px' class='Picture'>
+            </img></div><div class='infoPelis'><h2>${coleccionPintar.original_title}</h2><p>${coleccionPintar.overview}</p></div>`
+        console.log(coleccionPintar.poster_path);
+        console.log(coleccionPintar.overview);
+        console.log(coleccionPintar.original_title);
+    }
     return;
 };
 
